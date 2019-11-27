3f53f509f17e653711779a7b592703f3f6613f29
Alasdair Bennett <alasdairjbbennett@gmail.com>
2019-11-26 22:52:20 -0500

    Add UI for google sign in

--- scripts/main.js
+++
@@ -1,8 +0,0 @@
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
-
-ui.start('#firebaseui-auth-container', {
-    signInOptions: [
-        // List of OAuth providers supported.
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID
-    ]
-});
